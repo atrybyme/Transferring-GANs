5bcece47a4893ae488948a7cc4b53cdf474c1f8d
Shubhansh Awasthi <shubhansh@iitg.ernet.in>
2019-01-01 00:43:32 +0530

    Add files via upload

--- code/resnet_gan.py
+++ code/resnet_gan.py
@@ -1,4 +1,5 @@
-import sys, locale
+import sys
+import locale
 from os import path
 
 locale.setlocale(locale.LC_ALL, '')
@@ -17,17 +18,19 @@ from common.logger import Logger
 cfg = tf.app.flags.FLAGS
 
 tf.app.flags.DEFINE_string("sDataSet", "cifar10", "cifar10, mnist, toy")
-tf.app.flags.DEFINE_string("sResultTag", "gan_v0", "your tag for each test case")
+tf.app.flags.DEFINE_string("sResultTag", "gan_v0",
+                           "your tag for each test case")
 
 tf.app.flags.DEFINE_boolean("bLoadPre", False, "pretrained model to be loaded")
 tf.app.flags.DEFINE_string("sPreTag", "", "pretrained model tag")
 tf.app.flags.DEFINE_boolean("bLoadCheckpoint", False, "bLoadCheckpoint")
-tf.app.flags.DEFINE_string("sResultDir", SOURCE_DIR + "result/", "where to save the checkpoint and sample")
+tf.app.flags.DEFINE_string("sResultDir", SOURCE_DIR +
+                           "result/", "where to save the checkpoint and sample")
 
 tf.app.flags.DEFINE_boolean("bAMGAN", True, "")
 
-tf.app.flags.DEFINE_integer("iMaxIter", 500000, "")
-tf.app.flags.DEFINE_integer("iBatchSize", 1024, "")
+tf.app.flags.DEFINE_integer("iMaxIter", 50000, "")
+tf.app.flags.DEFINE_integer("iBatchSize", 50, "")
 
 tf.app.flags.DEFINE_integer("iTrainG", 1, "")
 tf.app.flags.DEFINE_integer("iTrainD", 1, "")
@@ -45,7 +48,7 @@ tf.app.flags.DEFINE_string("oAct", 'lrelu', "relu, lrelu, selu")
 tf.app.flags.DEFINE_integer("iDimsC", 3, "")
 tf.app.flags.DEFINE_integer("iDimsZ", 128, "")
 
-tf.app.flags.DEFINE_integer("iFilterDimsG", 64, "")
+tf.app.flags.DEFINE_integer("iFilterDimsG", 96, "")
 tf.app.flags.DEFINE_integer("iFilterDimsD", 64, "")
 
 tf.app.flags.DEFINE_float("fDropRate", 0.3, "")
@@ -101,7 +104,6 @@ def discriminator_dcgan(input, num_logits):
 
     with tf.variable_scope('discriminator', tf.AUTO_REUSE):
 
-
         h0 = basic_block(input, iFilterDimsD, ksize=3,
                          stride=1, name='basic_block')
         h0 = residual_block(h0, iFilterDimsD*2, ksize=3,
@@ -132,13 +134,14 @@ def residual_block_generator(input, filters, ksize, stride, name=None):
     conv_residual = batch_norm(h0, name=name+'conv_batch_norm1')
     conv_residual = activate(conv_residual, cfg.oAct)
     conv_residual = deconv2d(conv_residual, filters, ksize=ksize,
-                           stride=stride, name=name + 'deconv')
+                             stride=stride, name=name + 'deconv')
     conv_residual = batch_norm(conv_residual, name=name+'conv_batch_norm2')
     conv_residual = activate(conv_residual, cfg.oAct)
     conv_residual = conv2d(conv_residual, filters, ksize=ksize,
                            stride=1, name=name + 'conv')
-    identity = deconv2d(h0, filters, ksize=ksize, stride=stride, name=name+'identity')
-    
+    identity = deconv2d(h0, filters, ksize=ksize,
+                        stride=stride, name=name+'identity')
+
     return identity + conv_residual
 
 
@@ -156,10 +159,14 @@ def generator_dcgan(num_sample, z=None):
         h0 = linear(h0, 4 * 4 * (iFilterDimsG * 16))  # linear 4x4
         h0 = tf.reshape(h0, [-1, iFilterDimsG * 16, 4, 4])  # reshape 4x4
 
-        h0 = residual_block_generator(h0, iFilterDimsG * 8,ksize=3,stride=2,name='generator_block1_4x4')
-        h0 = residual_block_generator(h0, iFilterDimsG * 4,ksize=3,stride=2,name='generator_block2_8x8')
-        h0 = residual_block_generator(h0, iFilterDimsG * 2,ksize=3,stride=2,name='generator_block3_16x16')
-        h0 = residual_block_generator(h0, iFilterDimsG , ksize=3, stride=2, name='generator_block4_32x32')
+        h0 = residual_block_generator(
+            h0, iFilterDimsG * 8, ksize=3, stride=2, name='generator_block1_4x4')
+        h0 = residual_block_generator(
+            h0, iFilterDimsG * 4, ksize=3, stride=2, name='generator_block2_8x8')
+        h0 = residual_block_generator(
+            h0, iFilterDimsG * 2, ksize=3, stride=2, name='generator_block3_16x16')
+        h0 = residual_block_generator(
+            h0, iFilterDimsG, ksize=3, stride=2, name='generator_block4_32x32')
         h0 = batch_norm(h0, name='bn_64x64')
         h0 = activate(h0, cfg.oAct)
         h0 = conv2d(h0, cfg.iDimsC, ksize=3, stride=1, name='conv_64x64')
@@ -178,8 +185,6 @@ def generator_dcgan(num_sample, z=None):
         # h0 = tf.nn.tanh(h0)
 
         return h0
-
-
 
 
 ############################################################################################################################################
@@ -217,8 +222,10 @@ def param_count(gradient_value):
 def plot_generated_toy_data(X_gen, X_real, gen_iter, dir):
 
     plt.figure()
-    plt.scatter(X_gen[:5000, 0],  X_gen[:5000, 1], s=0.5, color="red", marker="o", label='fake_data')
-    plt.scatter(X_real[:5000, 0], X_real[:5000, 1], s=0.5, color="blue", marker="o", label='real_data')
+    plt.scatter(X_gen[:5000, 0],  X_gen[:5000, 1], s=0.5,
+                color="red", marker="o", label='fake_data')
+    plt.scatter(X_real[:5000, 0], X_real[:5000, 1], s=0.5,
+                color="blue", marker="o", label='real_data')
     plt.legend()
     plt.savefig(dir + "/train_iter%s.png" % gen_iter)
     plt.close()
@@ -245,15 +252,18 @@ def get_score(samples):
 
     if ref_icp_activations is None:
         logger.log('Evaluating Reference Statistic: icp_model')
-        ref_icp_preds, ref_icp_activations = icp_model.get_preds(data_X.transpose(0, 2, 3, 1))
-        logger.log('\nref_icp_score: %.3f\n' % InceptionScore.inception_score_H(ref_icp_preds)[0])
+        ref_icp_preds, ref_icp_activations = icp_model.get_preds(
+            data_X.transpose(0, 2, 3, 1))
+        logger.log('\nref_icp_score: %.3f\n' %
+                   InceptionScore.inception_score_H(ref_icp_preds)[0])
 
     # if ref_am_preds is None:
     #     logger.log('Evaluating Reference Statistic: am_model')
     #     ref_am_preds, ref_am_activations = am_model.get_preds(dataX.transpose(0, 2, 3, 1))
 
     logger.log('Evaluating Generator Statistic')
-    icp_preds, icp_activcations = icp_model.get_preds(samples.transpose(0, 2, 3, 1))
+    icp_preds, icp_activcations = icp_model.get_preds(
+        samples.transpose(0, 2, 3, 1))
     # am_preds, am_activations = am_model.get_preds(samples.transpose(0, 2, 3, 1), dataX.transpose(0, 2, 3, 1))
 
     icp_score = InceptionScore.inception_score_KL(icp_preds)
@@ -264,6 +274,7 @@ def get_score(samples):
     # return icp_score, am_score, fid
 
 ############################################################################################################################################
+
 
 dataX, dataY, testX, testY = load_dataset(cfg.sDataSet)
 # print(dataX.shape)
@@ -277,7 +288,8 @@ data_X = dataX
 # dataY = dataY[:500]
 data_gen = labeled_data_gen_epoch(dataX, dataY, cfg.iBatchSize)
 
-sTestName = (cfg.sResultTag + '_' if len(cfg.sResultTag) else "") + cfg.sDataSet
+sTestName = (cfg.sResultTag + '_' if len(cfg.sResultTag)
+             else "") + cfg.sDataSet
 
 sTestCaseDir = cfg.sResultDir + sTestName + '/'
 sSampleDir = sTestCaseDir + '/samples/'
@@ -314,11 +326,12 @@ sess = tf.Session(config=config)
 
 ############################################################################################################################################
 
-if cfg.sDataSet == 'cifar10' or cfg.sDataSet == 'mnist' or cfg.sDataSet=='imagenet':
+if cfg.sDataSet == 'cifar10' or cfg.sDataSet == 'mnist' or cfg.sDataSet == 'imagenet':
     generator = generator_dcgan
     discriminator = discriminator_dcgan
 
-    real_datas = tf.placeholder(tf.float32, [None, cfg.iDimsC, 64, 64], name='real_datas')
+    real_datas = tf.placeholder(
+        tf.float32, [None, cfg.iDimsC, 64, 64], name='real_datas')
     fake_datas = generator(cfg.iBatchSize)
 else:
     generator = generator_mlp
@@ -330,25 +343,31 @@ else:
 fake_labels = tf.placeholder(tf.int32, shape=[None])
 real_labels = tf.placeholder(tf.int32, shape=[None])
 
-num_logits = 11 if cfg.bAMGAN else 1
+num_logits = 1001 if cfg.bAMGAN else 1
 real_logits = discriminator(real_datas, num_logits)
 fake_logits = discriminator(fake_datas, num_logits)
 
 if cfg.bAMGAN:
 
-    dis_real_loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=real_logits, labels=real_labels))
-    dis_fake_loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=fake_logits, labels=tf.ones_like(real_labels)*1000))
+    dis_real_loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(
+        logits=real_logits, labels=real_labels))
+    dis_fake_loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(
+        logits=fake_logits, labels=tf.ones_like(real_labels)*1000))
 
     dis_gan_loss = dis_fake_loss + dis_real_loss
-    gen_gan_loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=fake_logits, labels=tf.stop_gradient(tf.to_int32(tf.arg_max(fake_logits[:, :1000], 1)))))
+    gen_gan_loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(
+        logits=fake_logits, labels=tf.stop_gradient(tf.to_int32(tf.arg_max(fake_logits[:, :1000], 1)))))
 
 else:
 
-    dis_real_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=real_logits, labels=tf.ones_like(real_logits)))
-    dis_fake_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=fake_logits, labels=tf.zeros_like(fake_logits)))
+    dis_real_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(
+        logits=real_logits, labels=tf.ones_like(real_logits)))
+    dis_fake_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(
+        logits=fake_logits, labels=tf.zeros_like(fake_logits)))
 
     dis_gan_loss = dis_fake_loss + dis_real_loss
-    gen_gan_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=fake_logits, labels=tf.ones_like(fake_logits)))
+    gen_gan_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(
+        logits=fake_logits, labels=tf.ones_like(fake_logits)))
 
 gen_total_loss = gen_gan_loss
 dis_total_loss = dis_gan_loss
@@ -360,16 +379,20 @@ dis_vars = [var for var in tot_vars if 'discriminator' in var.name]
 global_step = tf.Variable(0, trainable=False, name='global_step')
 
 if cfg.oDecay == 'linear':
-    lrD = cfg.fLrIniD * tf.maximum(0., 1. - (tf.cast(global_step, tf.float32) / cfg.iMaxIter))
+    lrD = cfg.fLrIniD * \
+        tf.maximum(0., 1. - (tf.cast(global_step, tf.float32) / cfg.iMaxIter))
 elif cfg.oDecay == 'exp':
-    lrD = tf.train.exponential_decay(cfg.fLrIniD, global_step, cfg.iMaxIter // 10, 0.5, True)
+    lrD = tf.train.exponential_decay(
+        cfg.fLrIniD, global_step, cfg.iMaxIter // 10, 0.5, True)
 else:
     lrD = cfg.fLrIniD
 
 if cfg.oDecay == 'linear':
-    lrG = cfg.fLrIniG * tf.maximum(0., 1. - (tf.cast(global_step, tf.float32) / cfg.iMaxIter))
+    lrG = cfg.fLrIniG * \
+        tf.maximum(0., 1. - (tf.cast(global_step, tf.float32) / cfg.iMaxIter))
 elif cfg.oDecay == 'exp':
-    lrG = tf.train.exponential_decay(cfg.fLrIniG, global_step, cfg.iMaxIter // 10, 0.5, True)
+    lrG = tf.train.exponential_decay(
+        cfg.fLrIniG, global_step, cfg.iMaxIter // 10, 0.5, True)
 else:
     lrG = cfg.fLrIniG
 
@@ -380,10 +403,13 @@ gen_optimizer = None
 # elif cfg.oOpt == 'mom':
 #     gen_optimizer = tf.train.MomentumOptimizer(learning_rate=lr, momentum=cfg.fBeta1, use_nesterov=True)
 # elif cfg.oOpt == 'adam':
-gen_optimizer = tf.train.AdamOptimizer(learning_rate=lrG, beta1=cfg.fBeta1, beta2=cfg.fBeta2, epsilon=cfg.fEpsilon)
+gen_optimizer = tf.train.AdamOptimizer(
+    learning_rate=lrG, beta1=cfg.fBeta1, beta2=cfg.fBeta2, epsilon=cfg.fEpsilon)
 
-gen_gradient_values = gen_optimizer.compute_gradients(gen_total_loss, var_list=gen_vars)
-gen_optimize_ops = gen_optimizer.apply_gradients(gen_gradient_values, global_step=global_step)
+gen_gradient_values = gen_optimizer.compute_gradients(
+    gen_total_loss, var_list=gen_vars)
+gen_optimize_ops = gen_optimizer.apply_gradients(
+    gen_gradient_values, global_step=global_step)
 
 dis_optimizer = None
 
@@ -392,9 +418,11 @@ dis_optimizer = None
 # elif cfg.oOpt == 'mom':
 #     dis_optimizer = tf.train.MomentumOptimizer(learning_rate=lr, momentum=cfg.fBeta1, use_nesterov=True)
 # elif cfg.oOpt == 'adam':
-dis_optimizer = tf.train.AdamOptimizer(learning_rate=lrD, beta1=cfg.fBeta1, beta2=cfg.fBeta2, epsilon=cfg.fEpsilon)
+dis_optimizer = tf.train.AdamOptimizer(
+    learning_rate=lrD, beta1=cfg.fBeta1, beta2=cfg.fBeta2, epsilon=cfg.fEpsilon)
 
-dis_gradient_values = dis_optimizer.compute_gradients(dis_total_loss, var_list=dis_vars)
+dis_gradient_values = dis_optimizer.compute_gradients(
+    dis_total_loss, var_list=dis_vars)
 dis_optimize_ops = dis_optimizer.apply_gradients(dis_gradient_values)
 
 saver = tf.train.Saver(max_to_keep=5)
@@ -407,12 +435,14 @@ last_save_time = last_log_time = last_plot_time = last_score_time = time.time()
 if cfg.bLoadPre:
     if cfg.sPreTag == "":
         assert False
-    load_dis_vars = [v for v in tf.trainable_variables() if 'discriminator' in v.name and 'linear' not in v.name]
-    load_dis_vars = [v for v in load_dis_vars if 'discriminator/conv32/' in v.name]
+    load_dis_vars = [v for v in tf.trainable_variables(
+    ) if 'discriminator' in v.name and 'linear' not in v.name]
+    load_dis_vars = [
+        v for v in load_dis_vars if 'discriminator/conv32/' in v.name]
     for var in load_dis_vars:
         print(var.name)
     loader_dis = tf.train.Saver(load_dis_vars)
-    
+
     print([v.name for v in load_dis_vars])
     ini_model(sess)
     try:
@@ -447,11 +477,14 @@ elif cfg.bLoadCheckpoint:
 else:
     ini_model(sess)
 
-fixed_noise = tf.constant(np.random.normal(size=(100, cfg.iDimsZ)).astype('float32'))
+fixed_noise = tf.constant(np.random.normal(
+    size=(100, cfg.iDimsZ)).astype('float32'))
 fixed_noise_gen = generator(100, fixed_noise)
 
-logger.log("Generator Total Parameter Count: {}".format(locale.format("%d", param_count(gen_gradient_values), grouping=True)))
-logger.log("Discriminator Total Parameter Count: {}".format(locale.format("%d", param_count(dis_gradient_values), grouping=True)))
+logger.log("Generator Total Parameter Count: {}".format(
+    locale.format("%d", param_count(gen_gradient_values), grouping=True)))
+logger.log("Discriminator Total Parameter Count: {}".format(
+    locale.format("%d", param_count(dis_gradient_values), grouping=True)))
 logger.log("\n\n")
 
 while iter <= cfg.iMaxIter:
@@ -462,7 +495,8 @@ while iter <= cfg.iMaxIter:
     for id in range(cfg.iTrainD):
         _datas, _labels = data_gen.__next__()
         _, _dis_total_loss, _dis_gan_loss, _lr, _real_logits, _fake_logits = sess.run(
-            [dis_optimize_ops, dis_total_loss, dis_gan_loss, lrG, real_logits, fake_logits],
+            [dis_optimize_ops, dis_total_loss, dis_gan_loss,
+                lrG, real_logits, fake_logits],
             feed_dict={real_datas: _datas, real_labels: _labels})
 
     for ig in range(cfg.iTrainG):
@@ -491,7 +525,7 @@ while iter <= cfg.iMaxIter:
     if (iter + 1) % 10000 == 0:
         icp_score, am_score, fid = get_score(gen_n_images(50000))
         logger.info('score_fid', fid)
-        ##logger.info('score_am', am_score)
+        logger.info('score_am', am_score)
         logger.info('score_icp', icp_score)
         last_score_time = time.time()
         logger.save()
@@ -505,14 +539,25 @@ while iter <= cfg.iMaxIter:
         else:
             save_images(_fixed_noise_gen.transpose(0, 2, 3, 1), [
                         10, 10], '{}/train_{:02d}_{:04d}.png'.format(sSampleDir, iter // 10000, iter % 10000))
-        ##last_plot_time = time.time()
+        last_plot_time = time.time()
 
-    ##if time.time() - last_save_time > 60*5:
-        
+    # if time.time() - last_save_time > 60*5:
+    #     logger.save()
+    #     save_model(saver, sess, sCheckpointDir, step=iter)
+    #     last_save_time = time.time()
+    #     logger.log('Model Saved\n\n')
 
-    # if time.time() - last_log_time > 6*1:
+    # # if time.time() - last_log_time > 6*1:
+
     logger.flush()
-    ##last_log_time = time.time()
+    last_log_time = time.time()
 
-    ##if time.time() - last_plot_time > 60*1:
-        
+    # if time.time() - last_plot_time > 60*1:
+    #     logger.plot()
+    #     _fixed_noise_gen = sess.run(fixed_noise_gen)
+    #     if cfg.sDataSet == 'toy':
+    #         plot_generated_toy_data(_fixed_noise_gen, _datas, iter, sSampleDir)
+    #     else:
+    #         save_images(_fixed_noise_gen.transpose(0, 2, 3, 1), [
+    #                     10, 10], '{}/train_{:02d}_{:04d}.png'.format(sSampleDir, iter // 10000, iter % 10000))
+    #     last_plot_time = time.time()
